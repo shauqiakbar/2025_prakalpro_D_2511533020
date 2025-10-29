90bbd5b71f24ccf46a51b29683e6be21fd198818
shauqiakbar <jakasro555@gmail.com>
2025-10-29 14:28:50 +0700

    Add files via upload

---
+++ prakalpro_2025_D_2511533020/src/pekan5/NestedFor0.java
@@ -0,0 +1,16 @@
+package pekan5;
+
+public class NestedFor0 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int line = 1; line <=5; line++) {
+			for (int j = 1; j <= (-1 * line + 5 ); j++) {
+				System.out.print(".");
+			}
+			System.out.print(line);
+			System.out.println();
+		}
+	}
+
+}

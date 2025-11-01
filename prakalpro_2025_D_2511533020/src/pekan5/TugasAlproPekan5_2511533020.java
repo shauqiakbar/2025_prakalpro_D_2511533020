8837e76b20280ea6a3028f170243fe36696fec73
shauqiakbar <jakasro555@gmail.com>
2025-11-01 12:38:45 +0700

    Add files via upload

---
+++ prakalpro_2025_D_2511533020/src/pekan5/TugasAlproPekan5_2511533020.java
@@ -0,0 +1,83 @@
+package alprotugas;
+
+public class TugasAlproPekan5_2511533020 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 System.out.println("#================#");
+
+		    
+	        System.out.print("|");
+	        	for (int i = 0; i < 6; i++) { 
+	            System.out.print(" ");
+	        }
+	        System.out.print("<><>");
+	        	for (int i = 0; i < 6 ; i++) { 
+	            System.out.print(" ");
+	        }
+	        System.out.println("|");
+
+	     
+	        System.out.print("|");
+	        	for (int i = 0; i < 4; i++) { 
+	            System.out.print(" ");
+	        }
+	        System.out.print("<>....<>");
+	        	for (int i = 0; i < 4; i++) { 
+	            System.out.print(" ");
+	        }
+	        System.out.println("|");
+
+
+	        System.out.print("|");
+	        	for (int i = 0; i < 2; i++) {
+	            System.out.print(" ");
+	        }
+	        System.out.print("<>........<>");
+	        	for (int i = 0; i < 2; i++) { 
+	            System.out.print(" ");
+	        }
+	        System.out.println("|");
+
+	              String barisTengah = "|<> .......... <>|";
+	        System.out.println(barisTengah);
+	        System.out.println(barisTengah); 
+
+	        System.out.print("|");
+	        	for (int i = 0; i < 2; i++) {
+	            System.out.print(" ");
+	        }
+	        System.out.print("<>........<>");
+	        	for (int i = 0; i < 2; i++) { 
+	            System.out.print(" ");
+	        }
+	        System.out.println("|");
+
+	        
+	      
+	        System.out.print("|");
+	        	for (int i = 0; i < 4; i++) { 
+	            System.out.print(" ");
+	        }
+	        System.out.print("<>....<>");
+	        	for (int i = 0; i < 4; i++) { 
+	            System.out.print(" ");
+	        }
+	        System.out.println("|");
+
+
+	        
+	        System.out.print("|");
+	        	for (int i = 0; i < 6; i++) { 
+	            System.out.print(" ");
+	        }
+	        System.out.print("<><>");
+	        	for (int i = 0; i < 6 ; i++) { 
+	            System.out.print(" ");
+	        }
+	        System.out.println("|");
+
+	      
+	        System.out.println("#================#");
+	    }
+	}
